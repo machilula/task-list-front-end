697624d92aef9a14ad54d55e31b579eaa770073b
Lula <mlusan95@gmail.com>
2024-12-12 19:59:01 -0500

    wave 3 done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,21 +1,31 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, onCompleteTask, onDeleteTask }) => {
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+
+  const onTaskToggle = () => {
+    onCompleteTask(id);
+  };
+
+  const onDeleteTaskClick = () => {
+    onDeleteTask(id);
+  };
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={onTaskToggle}
       >
         {title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button
+        className="tasks__item__remove button"
+        onClick={onDeleteTaskClick}
+      >x</button>
     </li>
   );
 };
@@ -24,6 +34,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onCompleteTask: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired
 };
 
 export default Task;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,30 +2,59 @@ import PropTypes from 'prop-types';
 import Task from './Task.jsx';
 import './TaskList.css';
 
-const TaskList = ({ tasks }) => {
-  const getTaskListJSX = (tasks) => {
-    return tasks.map((task) => {
-      return (
-        <Task
-          key={task.id}
-          id={task.id}
-          title={task.title}
-          isComplete={task.isComplete}
-        />
-      );
-    });
-  };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+// const TaskList = ({ tasks }) => {
+//   const getTaskListJSX = (tasks) => {
+//     return tasks.map((task) => {
+//       return (
+//         <Task
+//           key={task.id}
+//           id={task.id}
+//           title={task.title}
+//           isComplete={task.isComplete}
+//         />
+//       );
+//     });
+//   };
+//   return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+// };
+
+// TaskList.propTypes = {
+//   tasks: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       title: PropTypes.string.isRequired,
+//       isComplete: PropTypes.bool.isRequired,
+//     })
+//   ).isRequired,
+// };
+
+// export default TaskList;
+
+const TaskList = ({ taskData, onCompleteTask, onDeleteTask }) => {
+  const taskComponents = taskData.map((task) => {
+    return (
+      <Task
+        key={task.id}
+        id={task.id}
+        title={task.title}
+        isComplete={task.isComplete}
+        onCompleteTask={onCompleteTask}
+        onDeleteTask={onDeleteTask}
+      />);
+  });
+  return (<ul className="tasks__list no-bullet">{taskComponents}</ul>);
 };
 
 TaskList.propTypes = {
-  tasks: PropTypes.arrayOf(
+  taskData: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
       isComplete: PropTypes.bool.isRequired,
     })
   ).isRequired,
+  onCompleteTask: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired
 };
 
 export default TaskList;
