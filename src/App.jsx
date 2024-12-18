a10983976641da187013166166a5166e00803945
lioliadoc <liolia.doc@gmail.com>
2024-12-17 20:37:51 -0600

    Wave_4

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,83 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+const kbaseURL = 'http://127.0.0.1:5000';
+
+const convertFromApi = (apiTask) => {
+  const newTask = {
+    id: apiTask.id,
+    title: apiTask.title,
+    isComplete: apiTask.is_complete
+  };
+  return newTask;
+};
+
+const getAllTasksApi = () => {
+  return axios.get(`${kbaseURL}/tasks`)
+    .then( response => {
+      const apiTasks = response.data;
+      const newTasks = apiTasks.map(convertFromApi);
+      return newTasks;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+const toggleTaskCompletionApi = id => {
+  return axios.patch(`${kbaseURL}/tasks/${id}`)
+    .then( response => {
+      const newTask = convertFromApi(response.data);
+      return newTask;
+    })
+    .catch( error =>{
+      console.log(error);
+    });
+};
+
+const delTaskApi = id => {
+  return axios.delete(`${kbaseURL}/tasks/${id}`)
+    .catch( error => {
+      console.log(error);
+    });
+};
 
 const App = () => {
-  const [taskData, setTaskData] = useState(TASKS);
+  const [taskData, setTaskData] = useState([]);
+
+  const getAllTasks = () => {
+    getAllTasksApi()
+      .then(tasks => {
+        setTaskData(tasks);
+      });
+  };
+
+  useEffect(() => {
+    getAllTasks();
+  });
 
   const handleCompleteTask = (id) => {
-    setTaskData(taskData => taskData.map(task =>{
-      if (task.id === id) {
-        return {...task, isComplete: !task.isComplete};
-      } else {
-        return task;
-      }
-    }));
+    toggleTaskCompletionApi(id)
+      .then((apiTask) => {
+        setTaskData(taskData => taskData.map(task =>{
+          if (task.id === id) {
+            return {...task, isComplete: !task.isComplete};
+          } else {
+            return task;
+          }
+        }));
+      });
   };
 
   const handleDeleteTask = (id) => {
-    setTaskData(taskData => taskData.filter(task => {
-      return task.id !== id;
-    }));
+    delTaskApi(id)
+      .then(() => {
+        setTaskData(taskData => taskData.filter(task => {
+          return task.id !== id;
+        }));
+      });
   };
 
   return (
